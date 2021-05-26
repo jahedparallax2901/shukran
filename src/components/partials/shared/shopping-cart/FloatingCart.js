52892f0b3df3fec09879503ad9dc870c020d1a5d
parallax-abusaleh <abusaleh@parallaxlogic.net>
2021-05-26 16:30:52 +0600

    cart add design effect

--- src/components/partials/shared/shopping-cart/FloatingCart.js
+++ src/components/partials/shared/shopping-cart/FloatingCart.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { AiOutlineShopping } from 'react-icons/ai';
+import { BsChevronDoubleLeft } from 'react-icons/bs';
 import { connect } from "react-redux";
 import { handleShowShoppingCart } from "../../../../redux";
 
@@ -24,25 +25,30 @@ class FloatingCart extends Component {
 
     return (
       !isShowingShoppingCart && (
-        <button
-          onClick={handleShowShoppingCart}
-          className="asside-card-drower asside-card-drower-button"
-        >
-          <div className="asside-top">
-          <AiOutlineShopping />
-            <p>
-              <span>
-                {shoppingCart?.cartSummery?.total_prdoucts ||
-                  this.countTotalItems() ||
-                  0}
-              </span>{" "}
-              Item
-            </p>
+        <div className="asside-card-drower-area">
+          <button
+            onClick={handleShowShoppingCart}
+            className="asside-card-drower asside-card-drower-button"
+          >
+            <div className="asside-top">
+            <AiOutlineShopping />
+              <p>
+                <span>
+                  {shoppingCart?.cartSummery?.total_prdoucts ||
+                    this.countTotalItems() ||
+                    0}
+                </span>{" "}
+                Item
+              </p>
+            </div>
+            <div className="asside-body">
+              <span>৳{shoppingCart?.cartSummery?.total_amount || 0}</span>
+            </div>
+          </button>
+          <div className="cart-hover-effect">
+            <BsChevronDoubleLeft/>
           </div>
-          <div className="asside-body">
-            <span>৳{shoppingCart?.cartSummery?.total_amount || 0}</span>
-          </div>
-        </button>
+        </div>
       )
     );
   }
